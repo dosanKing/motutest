fdf3d18b696d71d05e8b45ad74c6554c68574269
DESKTOP-O2PN775\user1 <lg23831885@gmail.com>
2025-06-26 14:24:46 +0900

    motuTest 1.commit

---
+++ motutest/motutest/src/main/java/kr/or/iei/MotutestApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotutestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotutestApplication.class, args);
+	}
+
+}
